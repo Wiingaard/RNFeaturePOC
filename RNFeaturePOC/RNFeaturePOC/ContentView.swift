410dbc4894b7828dc705e0820ca048a6c3b96f43
Martin <martin.wiin@gmail.com>
2021-07-06 13:37:54 +0200

    Add basic native app

---
+++ RNFeaturePOC/RNFeaturePOC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RNFeaturePOC
+//
+//  Created by Martin Wiingaard on 06/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
